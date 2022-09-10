4c4cd42c99b89ab72343d25c34fc60751b2388d2
Pat groulx <patricedupere@gmail.com>
2022-09-10 11:19:37 -0400

    modif chat_client et chat_server

--- racecar_beacon/src/lab_chat_client.py
+++ racecar_beacon/src/lab_chat_client.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import socket
-
 HOST = '127.0.0.1'
 PORT = 65432
 
+ 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-
+s.send(b"hello")
+data = s.received(1024)
+print(data)
+s.close()
--- racecar_beacon/src/lab_chat_server.py
+++ racecar_beacon/src/lab_chat_server.py
@@ -2,9 +2,16 @@
 
 import socket
 
-HOST = '127.0.0.1'
+HOST = '10.0.1.31'
 PORT = 65432
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
+s.listen(1)
 
+while True:
+    (conn, addr) = s.accept()
+    data =conn.recv(1024)
+    if not data: break
+    conn.send(data+b"*")
+conn.close()
