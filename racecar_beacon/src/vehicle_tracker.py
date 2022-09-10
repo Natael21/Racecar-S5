0ccd5108c56f1cc7a4c5bbfafd9ee084f62ac845
Pat groulx <patricedupere@gmail.com>
2022-09-10 14:19:12 -0400

    modif vehicle_tracker

--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import socket
+from struct import *
 
-HOST = ''
+
+HOST = '127.0.0.255'
 # This process should listen to a different port than the RemoteRequest client.
 PORT = 65431
 
@@ -10,6 +12,21 @@ class PositionBroadcast:
     def __init__(self):
 
         print("allo")
+
+    def start(self):
+        while True:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            s.connect((HOST, PORT))
+            #s.listen(1)
+            data = s.recvfrom(16)
+            format = "fffxxxx"
+            cmd = unpack(format, data)
+            print(cmd)
+            #s.send(bytearray(self.ipAdress))
+            #self.msg_ROS = s.recv(1024)
+            #print(self.msg_ROS)
+            s.close()
 
 
 if __name__ == "__main__":
