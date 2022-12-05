63029fb01d394d101114bfd3b5a40b365b54491e
Patdup <patricedupere@gmail.com>
2022-12-05 15:46:14 -0500

    vel_ball_debug

--- racecar_behaviors/scripts/vel_ball.py
+++ racecar_behaviors/scripts/vel_ball.py
@@ -26,6 +26,7 @@ class VelBall:
     def __init__(self):
 
         self.goal = rospy.Subscriber('/goal', Float64MultiArray, self.goal_callback, queue_size=1)
+        self.object = rospy.Subscriber('object_detected', String, queue_size=1)
         self.vel_cmd = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
         self.goal_target = True
@@ -33,13 +34,13 @@ class VelBall:
 
     def goal_callback(self, goal_ball):
         if(self.goal_target):
-            if(0 > goal_ball.data[0] > 0):
+            if(0 > goal_ball.data[1] > 0):
                 rospy.loginfo('allo')
                 self.twist = Twist()
                 self.twist.linear.x = 1
-                self.twist.angular.z = goal_ball.data[0]
+                self.twist.angular.z = goal_ball.data[1]
                 self.vel_cmd.publish(self.twist)
-                if(0.75 < goal_ball.data[1] < 2):
+                if(0.75 < goal_ball.data[0] < 1.8):
                     self.goal_target = False
 
             else:
@@ -47,23 +48,18 @@ class VelBall:
                 self.twist.linear.x = 1
                 self.twist.angular.z = 0
                 self.vel_cmd.publish(self.twist)
-                if(0.75 < goal_ball.data[1] < 2):
+                if(0.75 < goal_ball.data[0] < 1.8):
                     self.goal_target = False
 
 
         else:
-            rospy.loginfo('allo')
             self.twist = Twist()
             self.twist.linear.x = 0
             self.twist.angular.z = 0
             self.vel_cmd.publish(self.twist)
+            time.sleep(5)
+
             
-
-        
-
-
-
-
 
 def main():
     rospy.init_node('vel_ball')
